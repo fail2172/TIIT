0840a780dfbdc6519e0944d00909676ca8e2a88b
Artsiom <solovyev768@gmail.com>
2020-04-15 23:00:15 +0300

    ;)

---
+++ Settlement_work/Settlement_work/Settlement_work.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <Windows.h>
+using namespace std;
+int row, col, time = 1, num_of_test, count_v;
+int s[20], up[20];
+bool result[20];
+vector<vector<int>> list(20);
+void dfs(int v, int p = -1) {
+	s[v] = up[v] = time++;
+	int children = 0;
+	for (int i = 0; i < list[v].size(); i++) {
+		int to = list[v][i];
+		if (s[to] == 0) {
+			dfs(to, v);
+			children++;
+			up[v] = min(up[v], up[to]);
+			if (up[to] >= s[v] && p != -1)
+				result[v] = true;
+		}
+		else if (to != p)
+			up[v] = min(up[v], s[to]);
+	}
+	if (p == -1 && children > 1)
+		result[v] = true;
+}
+int arr_to_list(int** arr, int j, int v) {
+	for (int i = 0; i < row; i++) {
+		if (i != v && arr[i][j] != 0)
+			return i;
+	}
+}
+int main() {
+	setlocale(LC_ALL, "ru");
+	ifstream file("1");
+	while (!file.eof()) {
+		time = 1;
+		num_of_test++;
+		fill(result, result + 20, false);
+		fill(s, s + 20, false);
+		fill(up, up + 20, false);
+		file >> row >> col;
+		//Создание матрицы инцидентности
+		int** arr = new int* [row];
+		for (int i = 0; i < row; i++)
+			arr[i] = new int[col];
+		//Заполнение матрицы из файла
+		for (int i = 0; i < row; i++)
+			for (int j = 0; j < col; j++)
+				file >> arr[i][j];
+		//Перевод матрицы в список векторов
+		for (int i = 0; i < row; i++)
+			for (int j = 0; j < col; j++) {
+				if (arr[i][j] != 0)
+					list[i].push_back(arr_to_list(arr, j, i));
+			}
+		//Удаление матрицы
+		for (int i = 0; i < row; i++)
+			delete[] arr[i];
+		delete[] arr;
+		dfs(0);
+		//Очистка списка векторов
+		for (int i = 0; i < row; i++)
+			list[i].clear();
+		cout << "TEST " << num_of_test << " :" << endl;
+		for (int i = 0; i < row; i++) {
+			if (result[i] == true)
+				count_v++;
+		}
+		cout << "Количество точек сочленения : " << count_v << endl;
+		//Вывод вершин сочленения
+		if (count_v > 0) {
+			cout << "Номера вершин сочленения :\n";
+			for (int i = 0; i < row; i++) {
+				if (result[i] == true)
+					cout << i + 1 << " ";
+			}cout << endl;
+		}cout << "--------------------" << endl;
+		count_v = 0;
+	}
+	file.close();
+}
---
+++ party/party/party.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+const int INF = 1e6,MAX_E=1e6,MAX_V=302;
+int sours, stock, cows, type_of_dishes,  max_dishes, count_edge;
+int end_of_edge[MAX_E], next_edge[MAX_E], first_edge[MAX_E],capacity[MAX_E],visit[MAX_V];
+void Add_Edge(int u, int v, int cap) {
+	//Прямое ребро
+	end_of_edge[count_edge] = v;
+	next_edge[count_edge] = first_edge[u];
+	first_edge[u] = count_edge;
+	capacity[count_edge++] = cap;
+	//Обратное ребро
+	end_of_edge[count_edge] = u;
+	next_edge[count_edge] = first_edge[v];
+	first_edge[v] = count_edge;
+	capacity[count_edge++] = 0;
+}
+int findFlow(int u, int flow) {
+	if (u == stock)return flow;
+	visit[u] = true;
+	for (int edge = first_edge[u]; edge != -1; edge = next_edge[edge]) {
+		int to = end_of_edge[edge];
+		if (!visit[to] && capacity[edge] > 0) {
+			int minflow = findFlow(to, min(flow, capacity[edge]));
+			if (minflow > 0) {
+				capacity[edge] -= minflow;
+				capacity[edge ^ 1] += minflow;
+				return minflow;
+			}
+		}
+	}
+	return 0;
+}
+int main() {
+	fill(first_edge, first_edge + MAX_E, -1);
+	ifstream file;
+	file.open("party.in");
+	file >> cows >> max_dishes >> type_of_dishes;
+	sours = cows + type_of_dishes + 1;
+	stock = cows + type_of_dishes + 2;
+ 	for (int i = 1; i <= cows; i++) {
+		Add_Edge(cows + type_of_dishes + 1, i, max_dishes);
+	}
+	for (int i = cows + 1; i <= cows + type_of_dishes; i++) {
+		int lim_of_dishes;
+		file >> lim_of_dishes;
+		Add_Edge(i, cows + type_of_dishes + 2, lim_of_dishes);
+	}
+	for (int i = 1; i <= cows; i++) {
+		int numb_of_dishes;
+		file >> numb_of_dishes;
+		for (int j = 0; j < numb_of_dishes; j++) {
+			int type_of_dish;
+			file >> type_of_dish;
+			Add_Edge(i,cows+ type_of_dish, 1);
+		}
+	}
+	file.close();
+	int maxflow = 0 , num_of_iteretions;
+	while (num_of_iteretions = findFlow(sours, INF) > 0) {
+		fill(visit, visit + MAX_V, false);
+		maxflow += num_of_iteretions;
+	}
+	cout << maxflow;
+	ofstream fout("party.out");
+	fout << maxflow;
+	fout.close();
+}
