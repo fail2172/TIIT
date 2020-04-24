cea1f79fea8af77fbed51d5e079b050196fca298
Artsiom <solovyev768@gmail.com>
2020-04-24 03:16:08 +0300

    ;)

--- Settlement_work/Settlement_work/Settlement_work.cpp
+++ Settlement_work/Settlement_work/Settlement_work.cpp
@@ -5,34 +5,38 @@
 using namespace std;
 int row, col, time = 1, num_of_test, count_v;
 int s[20], up[20];
+int** arr = new int* [20];
 bool result[20];
-vector<vector<int>> list(20);
-void dfs(int v, int p = -1) {
-	s[v] = up[v] = time++;
-	int children = 0;
-	for (int i = 0; i < list[v].size(); i++) {
-		int to = list[v][i];
-		if (s[to] == 0) {
-			dfs(to, v);
-			children++;
-			up[v] = min(up[v], up[to]);
-			if (up[to] >= s[v] && p != -1)
-				result[v] = true;
-		}
-		else if (to != p)
-			up[v] = min(up[v], s[to]);
-	}
-	if (p == -1 && children > 1)
-		result[v] = true;
-}
-int arr_to_list(int** arr, int j, int v) {
+int end_of_edge(int j, int v) {
 	for (int i = 0; i < row; i++) {
 		if (i != v && arr[i][j] != 0)
 			return i;
 	}
 }
+void dfs(int v, int p = -1) {
+	s[v] = up[v] = time++;  //прямой путь до вершины
+	int children = 0;//дети узла
+	for (int i = 0; i < col; i++) {
+		if (arr[v][i] != 0) {
+			int to = end_of_edge(i, v);  //следующий узел
+			if (s[to] == 0) {
+				dfs(to, v);//обход в глубину
+				children++;
+				up[v] = min(up[v], up[to]);//максимальная высота на которую можно подняться
+				if (up[to] >= s[v] && p != -1)
+					result[v] = true;//есили максимальная высота ниже либо равна пути к предку
+			}
+			else if (to != p)
+				up[v] = min(up[v], s[to]);//проверяем можем ли мы пойти выше
+		}
+	}
+	if (p == -1 && children > 1)
+		result[v] = true;//проверка корня
+}
 int main() {
 	setlocale(LC_ALL, "ru");
+	for (int i = 0; i < 20; i++)
+		arr[i] = new int[20];
 	ifstream file("1");
 	while (!file.eof()) {
 		time = 1;
@@ -40,29 +44,15 @@ int main() {
 		fill(result, result + 20, false);
 		fill(s, s + 20, false);
 		fill(up, up + 20, false);
+		for (int i = 0; i < 20; i++)
+			for (int j = 0; j < 20; j++)
+				arr[i][j] = 0;
 		file >> row >> col;
-		//Создание матрицы инцидентности
-		int** arr = new int* [row];
-		for (int i = 0; i < row; i++)
-			arr[i] = new int[col];
 		//Заполнение матрицы из файла
 		for (int i = 0; i < row; i++)
 			for (int j = 0; j < col; j++)
 				file >> arr[i][j];
-		//Перевод матрицы в список векторов
-		for (int i = 0; i < row; i++)
-			for (int j = 0; j < col; j++) {
-				if (arr[i][j] != 0)
-					list[i].push_back(arr_to_list(arr, j, i));
-			}
-		//Удаление матрицы
-		for (int i = 0; i < row; i++)
-			delete[] arr[i];
-		delete[] arr;
 		dfs(0);
-		//Очистка списка векторов
-		for (int i = 0; i < row; i++)
-			list[i].clear();
 		cout << "TEST " << num_of_test << " :" << endl;
 		for (int i = 0; i < row; i++) {
 			if (result[i] == true)
