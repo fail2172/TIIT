94b70faa7c78a96d76ee7fc746f7f7b4d76b80af
Artsiom <solovyev768@gmail.com>
2020-04-27 00:20:07 +0300

    27.04.2020

--- Settlement_work/Settlement_work/Settlement_work.cpp
+++ Settlement_work/Settlement_work/Settlement_work.cpp
@@ -3,24 +3,22 @@
 #include <vector>
 #include <Windows.h>
 using namespace std;
-int row, col, time = 1, num_of_test, count_v;
-int s[20], up[20];
-int** arr = new int* [20];
-bool result[20];
-int end_of_edge(int j, int v) {
+int end_of_edge(int j, int v, int** arr, int row) {
 	for (int i = 0; i < row; i++) {
 		if (i != v && arr[i][j] != 0)
 			return i;
 	}
 }
-void dfs(int v, int p = -1) {
+bool* dfs(int v, int** arr, int row, int col, int p = -1) {
+	static int s[20], up[20], time = 1;
+	static bool result[20];
 	s[v] = up[v] = time++;  //прямой путь до вершины
 	int children = 0;//дети узла
 	for (int i = 0; i < col; i++) {
 		if (arr[v][i] != 0) {
-			int to = end_of_edge(i, v);  //следующий узел
+			int to = end_of_edge(i, v, arr, row);  //следующий узел
 			if (s[to] == 0) {
-				dfs(to, v);//обход в глубину
+				dfs(to, arr, row, col, v);//обход в глубину
 				children++;
 				up[v] = min(up[v], up[to]);//максимальная высота на которую можно подняться
 				if (up[to] >= s[v] && p != -1)
@@ -32,27 +30,30 @@ void dfs(int v, int p = -1) {
 	}
 	if (p == -1 && children > 1)
 		result[v] = true;//проверка корня
+	if (p == -1) {
+		fill(s, s + 20, false);
+		fill(up, up + 20, false);
+		return result;
+	}
 }
 int main() {
 	setlocale(LC_ALL, "ru");
+	int** arr = new int* [20];
 	for (int i = 0; i < 20; i++)
 		arr[i] = new int[20];
+	int  num_of_test = 0, count_v = 0, row, col;
 	ifstream file("1");
 	while (!file.eof()) {
-		time = 1;
 		num_of_test++;
-		fill(result, result + 20, false);
-		fill(s, s + 20, false);
-		fill(up, up + 20, false);
 		for (int i = 0; i < 20; i++)
-			for (int j = 0; j < 20; j++)
-				arr[i][j] = 0;
+			fill(arr[i], arr[i] + 20, false);
 		file >> row >> col;
 		//Заполнение матрицы из файла
 		for (int i = 0; i < row; i++)
 			for (int j = 0; j < col; j++)
 				file >> arr[i][j];
-		dfs(0);
+		bool* result;
+		result = dfs(0, arr, row, col);
 		cout << "TEST " << num_of_test << " :" << endl;
 		for (int i = 0; i < row; i++) {
 			if (result[i] == true)
@@ -68,6 +69,7 @@ int main() {
 			}cout << endl;
 		}cout << "--------------------" << endl;
 		count_v = 0;
+		fill(result, result + 20, false);
 	}
 	file.close();
 }
